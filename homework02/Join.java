3c1013225c8627d45b08799f1af01422815af398
rivery811 <rivery811@gmail.com>
2019-08-19 09:38:59 +0900

    0819

--- homework02/Join.java
+++ homework02/Join.java
@@ -5,12 +5,12 @@ public class Join {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("===회원가입===");
-		String[] subject = {"아이디","비밀번호","이름","생년월일(예)1980-01-01",
+		String[] info = {"아이디","비밀번호","이름","생년월일(예)1980-01-01",
 				            "성인여부 (성인 true,미성년 false)","키 (소수점 첫째자리까지)",
 				            "몸무게(소수점 첫째자리까지)", "혈액형(A)"};  
 		String[] answer = new String[8];
-		for(int i= 0;i<subject.length;i++) {
-			System.out.printf(" %s: ",subject[i]);
+		for(int i= 0;i<info.length;i++) {
+			System.out.printf(" %s: ",info[i]);
 			answer[i]=scanner.next();
 		}
 		System.out.println("===회원정보===");
@@ -18,7 +18,7 @@ public class Join {
 			if(i==3) {
 			//모르겠음...
 			}else {
-			System.out.printf("%s:%s\n",subject[i],answer[i]);
+			System.out.printf("%s:%s\n",info[i],answer[i]);
 		}
 		
 		
--- homework02/LeapYear.java
+++ homework02/LeapYear.java
@@ -1,8 +1,11 @@
 package homework02;
-
+import java.util.Scanner;
 public class LeapYear {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("연도를 입력하세요");
+		int year = scanner.nextInt();
+		
 
 	}
 
